f21e8acc4c1ffe9ba0af4917e986ba69810dc493
Trine <trine@dekode.no>
2017-12-13 09:29:38 +0100

    Super simple default output

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -6,3 +6,29 @@
  */
 
 namespace Dekode\Hogan;
+
+/**
+ * Setup card data for different post types.
+ *
+ * @param array $card_raw Post ID and type.
+ * @return void
+ */
+function setup_card( $card_raw ) {
+
+	switch ( $card_raw['content_type'] ) {
+
+		// case 'savage-custom-card':
+		// 	break;
+		// post, page.
+		default:
+			$image = get_the_post_thumbnail( $card_raw['id'], 'post-thumbnail' );
+			if ( empty( $image ) ) {
+				$card = sprintf( '<a href="%s"><div>%s</div></a>', get_the_permalink( $card_raw['id'] ), get_the_title( $card_raw['id'] ) );
+			} else {
+				$card = sprintf( '<a href="%s">%s<div>%s</div></a>', get_the_permalink( $card_raw['id'] ), get_the_post_thumbnail( $card_raw['id'], 'post-thumbnail' ) , get_the_title( $card_raw['id'] ) );
+			}
+			break;
+	}
+
+	echo $card;
+}
