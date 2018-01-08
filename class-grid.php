40b949ab26f5c3999dc5ae636f8c3d47448108e8
Petter Walbø Johnsgård <petter@dekode.no>
2018-01-08 14:50:48 +0100

    Cleanup code

--- class-grid.php
+++ class-grid.php
@@ -61,22 +61,21 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Grid' ) && class_exists( '\\Dekode\\Hogan
 			// Heading field can be disabled using filter hogan/module/grid/heading/enabled (true/false).
 			hogan_append_heading_field( $fields, $this );
 
-			array_push( $fields,
-			[
-				'key' => $this->field_key . '_flex',
-				'label' => '',
-				'name' => 'flex_grid',
-				'type' => 'flexible_content',
+			array_push( $fields, [
+				'key'          => $this->field_key . '_flex',
+				'label'        => '',
+				'name'         => 'flex_grid',
+				'type'         => 'flexible_content',
 				'button_label' => esc_html__( 'Add cards', 'hogan-grid' ),
-				'wrapper' => [
+				'wrapper'      => [
 					'class' => 'grid-layouts',
 				],
-				'layouts' => [
+				'layouts'      => [
 					[
-						'key' => $this->field_key . '_flex_static_content',
-						'name' => 'static_content',
-						'label' => esc_html__( 'Static content', 'hogan-grid' ),
-						'display' => 'block',
+						'key'        => $this->field_key . '_flex_static_content',
+						'name'       => 'static_content',
+						'label'      => esc_html__( 'Static content', 'hogan-grid' ),
+						'display'    => 'block',
 						'sub_fields' => [
 							[
 								'type'          => 'button_group',
@@ -95,30 +94,30 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Grid' ) && class_exists( '\\Dekode\\Hogan
 								'return_format' => 'value',
 							],
 							[
-								'type'              => 'relationship',
-								'key'               => $this->field_key . '_posts_list',
-								'label'             => __( 'Pick items from list', 'hogan-grid' ),
-								'name'              => 'posts_list',
-								'value'             => null,
-								'instructions'      => __( 'Select items to by clicking the items on the left side', 'hogan-grid' ),
-								'required'          => 1,
-								'post_type'         => apply_filters( 'hogan/module/grid/static_content_post_types', [ 0 => 'post', 1 => 'page' ], $this ),
-								'taxonomy'          => apply_filters( 'hogan/module/grid/static_content_taxonomy', [], $this ),
-								'filters'           => apply_filters( 'hogan/module/grid/static_content_relation_filters', [ 0 => 'search',	1 => 'post_type', 2 => 'taxonomy',], $this ),
-								'elements'          => [
+								'type'          => 'relationship',
+								'key'           => $this->field_key . '_posts_list',
+								'label'         => __( 'Pick items from list', 'hogan-grid' ),
+								'name'          => 'posts_list',
+								'value'         => null,
+								'instructions'  => __( 'Select items to by clicking the items on the left side', 'hogan-grid' ),
+								'required'      => 1,
+								'post_type'     => apply_filters( 'hogan/module/grid/static_content_post_types', [ 0 => 'post', 1 => 'page' ], $this ),
+								'taxonomy'      => apply_filters( 'hogan/module/grid/static_content_taxonomy', [], $this ),
+								'filters'       => apply_filters( 'hogan/module/grid/static_content_relation_filters', [ 0 => 'search',	1 => 'post_type', 2 => 'taxonomy',], $this ),
+								'elements'      => [
 									0 => 'featured_image',
 								],
-								'min'               => 1,
-								'max'               => 10,
-								'return_format'     => 'id',
+								'min'           => 1,
+								'max'           => 10,
+								'return_format' => 'id',
 							],
 						],
 					],
 					[
-						'key' => $this->field_key . '_flex_dynamic_content',
-						'name' => 'dynamic_content',
-						'label' => esc_html__( 'Dynamic content', 'hogan-grid' ),
-						'display' => 'block',
+						'key'        => $this->field_key . '_flex_dynamic_content',
+						'name'       => 'dynamic_content',
+						'label'      => esc_html__( 'Dynamic content', 'hogan-grid' ),
+						'display'    => 'block',
 						'sub_fields' => [
 							[
 								'type'          => 'button_group',
@@ -137,52 +136,51 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Grid' ) && class_exists( '\\Dekode\\Hogan
 								'return_format' => 'value',
 							],
 							[
-								'type' => 'select',
-								'key' => $this->field_key . 'dynamic_card_content_type',
-								'label' => __( 'Content Type', 'hogan-grid' ),
-								'name' => 'card_content_type',
-								'instructions' => __( 'Select the content type to build cards from', 'hogan-grid' ),
-								'required' => 1,
-								'wrapper' => array(
+								'type'          => 'select',
+								'key'           => $this->field_key . 'dynamic_card_content_type',
+								'label'         => __( 'Content Type', 'hogan-grid' ),
+								'name'          => 'card_content_type',
+								'instructions'  => __( 'Select the content type to build cards from', 'hogan-grid' ),
+								'required'      => 1,
+								'wrapper'       => [
 									'width' => '50',
-								),
-								'choices' => apply_filters( 'hogan/module/grid/dynamic_content_post_types', [ 'post' => __( 'Posts', 'hogan-grid' ), 'page' => __( 'Pages', 'hogan-grid' ) ], $this ),
-								'allow_null' => 0,
-								'multiple' => 0,
-								'ui' => 0,
-								'ajax' => 0,
+								],
+								'choices'       => apply_filters( 'hogan/module/grid/dynamic_content_post_types', [ 'post' => __( 'Posts', 'hogan-grid' ), 'page' => __( 'Pages', 'hogan-grid' ) ], $this ),
+								'allow_null'    => 0,
+								'multiple'      => 0,
+								'ui'            => 0,
+								'ajax'          => 0,
 								'return_format' => 'value',
 							],
 							[
-								'type'              => 'number',
-								'key'               => $this->field_key . '_number_of_items',
-								'label'             => __( 'Number of items', 'hogan-grid' ),
-								'name'              => 'number_of_items',
-								'instructions'      => __( 'Set the number of items to display', 'hogan-grid' ),
-								'required'          => 1,
-								'default_value'     => 3,
-								'min'               => 1,
-								'max'               => 10,
-								'step'              => 1,
-								'wrapper'           => [
+								'type'          => 'number',
+								'key'           => $this->field_key . '_number_of_items',
+								'label'         => __( 'Number of items', 'hogan-grid' ),
+								'name'          => 'number_of_items',
+								'instructions'  => __( 'Set the number of items to display', 'hogan-grid' ),
+								'required'      => 1,
+								'default_value' => 3,
+								'min'           => 1,
+								'max'           => 10,
+								'step'          => 1,
+								'wrapper'       => [
 									'width' => '50',
 								],
 							],
 						],
 					],
 				],
-			]
-			);
+			] );
 
 			return $fields;
 		}
 
 		/**
-		 * Map fields to object variable.
+		 * Map raw fields from acf to object variable.
 		 *
-		 * @param array $content The content value.
-		 *
-		 * @return bool Whether validation of the module is successful / filled with content.
+		 * @param array $raw_content Content values.
+		 * @param int   $counter Module location in page layout.
+		 * @return void
 		 */
 		public function load_args_from_layout_content( array $raw_content, int $counter = 0 ) {
 
@@ -193,10 +191,12 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Grid' ) && class_exists( '\\Dekode\\Hogan
 
 		public function structure_card_data( $data ) {
 
-			if ( empty( $data ) ){
+			if ( empty( $data ) ) {
 				return '';
 			}
+
 			$cards = [];
+
 			foreach ( $data as $group ) {
 
 				switch ( $group['acf_fc_layout'] ) {
@@ -205,9 +205,9 @@ if ( ! class_exists( '\\Dekode\\Hogan\\Grid' ) && class_exists( '\\Dekode\\Hogan
 						foreach ( $group['posts_list'] as $card ) {
 
 							$cards[] = [
-								'id' => $card,
+								'id'           => $card,
 								'content_type' => get_post_type( $card ),
-								'style' => $group['card_style'],
+								'style'        => $group['card_style'],
 							];
 						}
 						break;
