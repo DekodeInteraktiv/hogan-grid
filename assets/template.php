2c693cbd1c926e32758364d4acfe93b4c0e9bc4f
Stian <stian@dekode.no>
2018-09-17 09:12:56 +0200

    Add filters for grid and item class names, Fixes #60

--- assets/template.php
+++ assets/template.php
@@ -18,19 +18,29 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Grid ) || ! function_exists(
 	return;
 }
 
-$classnames = hogan_classnames( 'hogan-grid', 'hogan-grid-text-' . $this->text_align, 'hogan-grid-size-' . $this->grid_size );
+$classnames = hogan_classnames( apply_filters( 'hogan/module/grid/grid_outer_classes', [
+	'hogan-grid',
+	'hogan-grid-text-' . $this->text_align,
+	'hogan-grid-size-' . $this->grid_size,
+], $this ) );
 ?>
 <div class="<?php echo esc_attr( $classnames ); ?>">
-	<div class="hogan-grid-inner">
+
+	<?php
+	$classnames = hogan_classnames( apply_filters( 'hogan/module/grid/grid_inner_classes', [
+		'hogan-grid-inner',
+	], $this ) );
+	?>
+
+	<div class="<?php echo esc_attr( $classnames ); ?>">
 		<?php
 		foreach ( $this->collection as $card_args ) :
 			$card       = savage_get_card( $card_args );
-			$classnames = hogan_classnames(
+			$classnames = hogan_classnames( apply_filters( 'hogan/module/grid/item_classes', array_merge( [
 				'hogan-grid-item',
 				'hogan-grid-item-size-' . $card_args['size'],
 				'hogan-grid-item-type-' . $card_args['type'],
-				$card['classnames']
-			);
+			], $card['classnames'] ), $card, $this ) );
 			?>
 			<div class="<?php echo esc_attr( $classnames ); ?>">
 				<div class="hogan-grid-item-inner">
